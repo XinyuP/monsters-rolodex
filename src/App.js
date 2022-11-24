416ec11a096ed4e12e1ce4498b357e0075972819
XinyuP <xinyup@umich.edu>
2022-11-24 18:30:18 -0500

    class component(constructor, state) and lifecycle method(componentDidMount())

--- src/App.js
+++ src/App.js
@@ -1,45 +1,55 @@
-import { useState, useEffect } from 'react';
-
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
+import { render } from '@testing-library/react';
 
-const App = () => {
-  const [searchField, setSearchField] = useState('');
-  const [monsters, setMonsters] = useState([]);
-  const [filteredMonsters, setFilterMonsters] = useState(monsters);
+class App extends Comment {
+    constructor() {
+      super();
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => setMonsters(users));
-  }, []);
+      this.state = {
+        monsters: [],
+        searchField: '',
+      };
+    }
 
-  useEffect(() => {
-    const newFilteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    });
+    // lifecycle method 
+    // whatever code in here will get runned when the component mount (the first time a component get placed on DOM)
+    // mounting: the first time react render the component onto the page, only happen once through out the component's life 
+    // the only time when a component remount is if it is unmounted (completely removed from DOM)
+    componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users') // a promise(async) --> eventually I am gonna have a value 
+        .then((response) => response.json()) // convert response to json // every .then() that return a value is gonna return another promise that hasn't been resolved 
+        .then((users) => 
+          this.setState(() => { // using function so that you can pass call back 
+            return {monsters: users}; 
+          },
+          () => {
+            // console.log(this.state)
+            // call back function
+          }
+          )
+          
+        );
+    }
+  
 
-    setFilterMonsters(newFilteredMonsters);
-  }, [monsters, searchField]);
 
-  const onSearchChange = (event) => {
-    const searchFieldString = event.target.value.toLocaleLowerCase();
-    setSearchField(searchFieldString);
-  };
+  render() {
+     return (
+      <div className='App'>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
 
-  return (
-    <div className='App'>
-      <h1 className='app-title'>Monsters Rolodex</h1>
-
-      <SearchBox
-        className='monsters-search-box'
-        onChangeHandler={onSearchChange}
-        placeholder='search monsters'
-      />
-      <CardList monsters={filteredMonsters} />
-    </div>
-  );
+  }
+ 
 };
 
 export default App;
