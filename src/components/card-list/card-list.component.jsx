40947c016868731fa4917b65873934d8bf8d3178
XinyuP <xinyup@umich.edu>
2022-11-24 15:56:38 -0500

    initial monster rolodex

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+class CardList extends Component {
+    render() {
+        const { monsters } = this.props; // destructure
+
+        return (
+            <div>
+            {
+                monsters.map((monster) => {
+                    <div key={monster.id}>
+                        <h1>{monster.name}</h1>
+                    </div>
+                })
+            }
+            </div>
+        )
+    }
+}
+
+export default CardList;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,30 @@
+// import { Component } from "react";
+
+// import './search-box.styles.css'
+// class SearchBox extends Component {
+//   render() {
+//     return (
+//       <input
+//         className={this.props.className}
+//         type="search"
+//         palceholder={this.props.palceholder}
+//         onChange={this.props.onChangeHandler}
+//       />
+//     );
+//   }
+// }
+// export default SearchBox;
+
+
+import './search-box.styles.css';
+
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+  <input
+    className={`search-box ${className}`}
+    type='search'
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+
+export default SearchBox;
