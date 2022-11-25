9fee008aed3b436e78363bec30f31a0e6c1e6d15
XinyuP <xinyup@umich.edu>
2022-11-25 00:46:03 -0500

    convert card list to functional component version

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,22 +1,40 @@
-import { Component } from "react";
 import Card from "../card/card.component";
 import "./card-list.styles.css";
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props; // destructure
 
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({monsters}) => (
+  //return (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <Card monster={monster} />;
+    })}
+  </div>
+  //);
+  // implicit return
+  
+)
 
 export default CardList;
+ 
+// import { Component } from "react";
+// import Card from "../card/card.component";
+// import "./card-list.styles.css";
 
+// class CardList extends Component {
+//   render() {
+//     const { monsters } = this.props; // destructure
+
+//     return (
+//       <div className="card-list">
+//         {monsters.map((monster) => {
+//           return <Card monster={monster} />;
+//         })}
+//       </div>
+//     );
+//   }
+// }
+
+// export default CardList;
+ 
 
 
 // import { Component } from "react";
