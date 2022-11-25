7a666debd24fb4c5aaad3c57488e7736a53b735e
XinyuP <xinyup@umich.edu>
2022-11-24 22:36:29 -0500

    add Card component

--- src/App.js
+++ src/App.js
@@ -57,6 +57,9 @@ class App extends Comment {
 
     return (
       <div className="App">
+        <h1 className="app-title">
+          Monsters Rolodex
+        </h1>
         {/* <input
           className="search-box"
           type="search"
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,15 @@
 import { Component } from "react";
+import Card from "../card/card.component";
 import "./card-list.styles.css";
 class CardList extends Component {
   render() {
     const { monsters } = this.props; // destructure
+
     return (
       <div className="card-list">
-        {monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        {monsters.map((monster) => {
+          return <Card monster={monster} />;
+        })}
       </div>
     );
   }
@@ -16,21 +18,21 @@ class CardList extends Component {
 export default CardList;
 
 
+
 // import { Component } from "react";
-// import Card from "../card/card.component";
 // import "./card-list.styles.css";
 // class CardList extends Component {
 //   render() {
 //     const { monsters } = this.props; // destructure
-
 //     return (
 //       <div className="card-list">
-//         {monsters.map((monster) => {
-//           return <Card monster={monster} />;
-//         })}
+//         {monsters.map(monster => (
+//           <h1 key={monster.id}>{monster.name}</h1>
+//         ))}
 //       </div>
 //     );
 //   }
 // }
 
 // export default CardList;
+
