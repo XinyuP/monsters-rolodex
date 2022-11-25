ad3cf2fc17836666b8b82dd1e38035cd646e738c
XinyuP <xinyup@umich.edu>
2022-11-25 00:53:08 -0500

    convert SearchBox to functional component version

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,3 +1,19 @@
+import "./search-box.styles.css";
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+export default SearchBox;
+
+
+
+
+/*
+
 import { Component } from "react";
 import './search-box.styles.css'
 // this file is not in isolation from the rest of our application
@@ -20,16 +36,4 @@ export default SearchBox;
 // no matter where you inport CSS, it will be present on the entire page 
 
 
-// import "./search-box.styles.css";
-
-// const SearchBox = ({ className, placeholder, onChangeHandler }) => (
-//   <input
-//     className={`search-box ${className}`}
-//     type="search"
-//     placeholder={placeholder}
-//     onChange={onChangeHandler}
-//   />
-// );
-
-// export default SearchBox;
-
+*/
